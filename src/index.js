d57e123834367ab25f831d03336af8745ba7264a
shrutiagarwal21 <khushiagarwal0204@gmail.com>
2025-04-03 00:40:31 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./app";
+import App from "./App-1";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
